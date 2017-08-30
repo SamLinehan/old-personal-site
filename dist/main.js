bc6843b15b805c13ffd735003ca8790d746e069f
Samuel <samuel.linehan15@gmail.com>
2017-08-29 21:24:30 -0400

    working on routing

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const Home = { template: '<div>Home</div>' };
 const About = { template: '<div>About</div>' };
 const Portfolio = { template: '<div>Portfolio</div>' };
 const Contact = { template: '<div>Contact</div>' };
 
-const routes = [{ path: '/about', component: About }, { path: '/portfolio', component: Portfolio }, { path: '/contact', component: Contact }];
+const routes = [{ path: '/', component: Home,
+  children: [{ path: '/about', component: About }, { path: '/portfolio', component: Portfolio }, { path: '/contact', component: Contact }] }, { path: '*', redirect: '/' }];
 
 const router = new VueRouter({
   routes // short for `routes: routes`
--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
 'use strict'
 
+const Home = { template: '<div>Home</div>' }
 const About = { template: '<div>About</div>' }
 const Portfolio = { template: '<div>Portfolio</div>' }
 const Contact = { template: '<div>Contact</div>'}
 
 const routes = [
-  { path: '/about', component: About },
-  { path: '/portfolio', component: Portfolio },
-  { path: '/contact', component: Contact}
+  { path: '/', component: Home,
+    children: [
+        { path: '/about', component: About },
+        { path: '/portfolio', component: Portfolio },
+        { path: '/contact', component: Contact}
+    ]},
+    { path: '*', redirect: '/' }
 ]
 
 const router = new VueRouter({
   routes // short for `routes: routes`
 })
+
 
 const app = new Vue({
   router,
