e284e92f73081e4e94edd859cf9b462db01bf719
Samuel <samuel.linehan15@gmail.com>
2017-09-05 19:35:11 -0400

    fixed routing

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 'use strict'
 
+
 const Home = { template: '<div>Home</div>' }
 const About = { template: '<div>About</div>' }
 const Portfolio = { template: '<div>Portfolio</div>' }
 const Contact = { template: '<div>Contact</div>'}
 
 const routes = [
-  { path: '/', component: Home,
-    children: [
-        { path: '/about', component: About },
-        { path: '/portfolio', component: Portfolio },
-        { path: '/contact', component: Contact}
-    ]},
-    { path: '*', redirect: '/' }
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  { path: '/portfolio', component: Portfolio },
+  { path: '/contact', component: Contact},
+  { path: '*', redirect: '/' }
 ]
 
 const router = new VueRouter({
@@ -21,8 +20,5 @@ const router = new VueRouter({
 
 
 const app = new Vue({
-  router,
-  data: {
-      header: 'SAM LINEHAN'
-  }
+  router
 }).$mount('#app')
